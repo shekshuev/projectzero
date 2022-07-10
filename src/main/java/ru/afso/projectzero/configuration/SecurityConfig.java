0a86d8ad726d6cd66da51379c53047ba6e8b5609
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2022-07-11 00:13:33 +0300

    cors fix

--- src/main/java/ru/afso/projectzero/configuration/SecurityConfig.java
+++ src/main/java/ru/afso/projectzero/configuration/SecurityConfig.java
@@ -46,7 +46,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
                 .anyRequest().authenticated()
                 .and()
                 .addFilterAfter(filter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling().authenticationEntryPoint(new RestAuthenticationEntryPoint());
+                .exceptionHandling().authenticationEntryPoint(new RestAuthenticationEntryPoint())
+                .and().cors();
     }
 
     @Override
